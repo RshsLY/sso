e4053f6c6e4307af8d15c2048dd6c6c29708853a
rshs <846844910@qq.com>
2023-07-24 16:58:20 +0800

    init

---
+++ sso-client/src/test/java/com/ly/ssoclient/MallSsoClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ly.ssoclient;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSsoClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
